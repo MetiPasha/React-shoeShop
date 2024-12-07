7b6c61e409deafa44e38e0cc19ac39d8179435dd
pasha <mtipasha2021@gmail.com>
2024-12-07 15:42:41 +0330

    lets start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, Tailwind with Vite!
+      </h1>
+    </div>
+  );
+}
