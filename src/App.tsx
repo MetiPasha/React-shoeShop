e896261d69d7d26809f93a4f68ba319868900e3f
pasha <mtipasha2021@gmail.com>
2024-12-08 16:47:30 +0330

    components created for Loding&Onvoarding and we rename our branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Loading from "./components/Loading";
 
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-green-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Hello, Tailwind with Vite!
-      </h1>
+    <div>
+      <Loading />
     </div>
   );
 }
