fd6f560b3d6bba8d5919b44aed74bb6237a96f91
pasha <mtipasha2021@gmail.com>
2024-12-10 13:15:20 +0330

    Login is complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Container from "./components/Container";
+import Login from "./components/Login/Login";
+import Register from "./components/Login/Register";
 
 function App() {
   return (
     <div>
-      <Container />
+      {/* <Container /> */}
+      <Login />
+      <Register />
     </div>
   );
 }
