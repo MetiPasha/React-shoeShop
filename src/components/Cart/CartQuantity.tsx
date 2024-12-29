bca84292d980abf827dae7da8806d49877988943
pasha <mtipasha2021@gmail.com>
2024-12-29 15:16:54 +0330

    cartQuantity just added

---
+++ src/components/Cart/CartQuantity.tsx
@@ -0,0 +1,42 @@
+interface CartQuantityProps {
+  value: number;
+  min: number;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CartQuantity: React.FC<CartQuantityProps> = ({
+  value,
+  min,
+  onChange,
+}) => {
+  return (
+    <div>
+      <div className="flex w-20 rounded-xl bg-gray-300 items-center justify-between gap-2">
+        <button
+          className="text-2xl p-1 font-semibold"
+          onClick={() =>
+            value > min &&
+            onChange({
+              target: { value: (value - 1).toString() },
+            } as React.ChangeEvent<HTMLInputElement>)
+          }
+        >
+          -
+        </button>
+        <p className="text-xl p-1 font-semibold">{value}</p>
+        <button
+          className="text-2xl p-1 font-semibold"
+          onClick={() =>
+            onChange({
+              target: { value: (value + 1).toString() },
+            } as React.ChangeEvent<HTMLInputElement>)
+          }
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartQuantity;
