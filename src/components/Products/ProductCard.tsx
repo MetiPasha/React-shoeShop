0c2d8023d95e16c812d7d044e8f122afbc7dd568
pasha <mtipasha2021@gmail.com>
2025-01-10 09:42:14 +0330

    change at productCard

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,41 +1,26 @@
-import { FC, ReactElement } from "react";
+import { FC } from "react";
+import { MdDelete } from "react-icons/md";
+import { IProductProps } from "../../type";
 
-export interface IProductProps {
-  id: number;
-  title: string;
-  price: number;
-  src: string;
-  images: {
-    title: {
-      src: string;
-    };
-  };
-  onClick: () => void;
-}
-
-const ProductCard: FC<IProductProps> = ({
-  title,
-  price,
-  images,
-  onClick,
-}): ReactElement => {
+const ProductCard: FC<IProductProps> = ({ name, price, images, onClick }) => {
   return (
     <div
       onClick={onClick}
       className="bg-white mt-4 col-span-1 cursor-pointer border-[1.2px] border-slate-200 rounded-sm p-2 transition text-center text-sm hover:scale-105"
     >
       {/* Product Image */}
-      <div className="flex flex-col items-center w-full gap-1">
+      <div className="flex flex-col  items-center w-full gap-1">
         <img
-          src={images}
-          alt={title}
+          src={images[0]}
+          alt={name}
           className="w-full h-full object-contain "
         />
+        <MdDelete className="flex text-xl text-white " />
       </div>
 
       {/* Product Info */}
       <div className="p-4 mt-4">
-        <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
+        <h3 className="text-lg font-semibold text-gray-800">{name}</h3>
         <p className="text-xl font-bold text-gray-900 mt-2">${price}</p>
       </div>
     </div>
